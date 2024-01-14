6184b42eac82d5732b622b4871b055745c6683f1
briannaclarkso <brianna.extra@gmail.com>
2024-01-14 13:28:37 -0600

    task 4

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,52 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId(){
+
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        assertNotEquals(job1.getId(), job2.getId());
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+
+        Job jobTest = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+//        String nameTest = "Product tester";
+
+        assertEquals("Product tester", jobTest.getName());
+        assertEquals("ACME", jobTest.getEmployer().getValue());
+        assertEquals("Desert", jobTest.getLocation().getValue());
+        assertEquals("Quality control", jobTest.getPositionType().getValue());
+        assertEquals("Persistence", jobTest.getCoreCompetency().getValue());
+
+        assertTrue(jobTest.getName() instanceof String);
+        assertTrue(jobTest.getEmployer() instanceof Employer);
+        assertTrue(jobTest.getLocation() instanceof Location);
+        assertTrue(jobTest.getPositionType() instanceof PositionType);
+        assertTrue(jobTest.getCoreCompetency() instanceof CoreCompetency);
+
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job jobTest1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job jobTest2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+
+        assertNotEquals(jobTest1.getId(), jobTest2.getId());
+
+    }
+
 }
